60709a91b8c595ffa8ce2ec97eed2e3ac912399d
zhangchunsheng <peter@Wozllas-MacBook.local>
2013-12-14 05:10:05 +0800

    add code

--- shenglong-electricv/platforms/ios/www/js/main.js
+++ shenglong-electricv/platforms/ios/www/js/main.js
@@ -1,8 +1,19 @@
-require(["helper/util"], function(util) {
-    //This function is called when scripts/helper/util.js is loaded.
-    //If util.js calls define(), then this function is not fired until
-    //util's dependencies have loaded, and the util argument will hold
-    //the module value for "helper/util".
-    console.log("test");
-        util.test();
+requirejs.config({
+    //By default load any module IDs from js/lib
+    baseUrl: 'js/lib',
+    //except, if the module ID starts with "app",
+    //load it from the js/app directory. paths
+    //config is relative to the baseUrl, and
+    //never includes a ".js" extension since
+    //the paths config could be for a directory.
+    paths: {
+        app: '../app',
+		jquery: 'jquery-1.9.1',
+		util: '../helper/util',
+		jquery-mobile: 'jquery.mobile-1.3.2'
+    }
 });
+
+requirejs(["app/app", "util"], function(app, util) {
+	
+})
