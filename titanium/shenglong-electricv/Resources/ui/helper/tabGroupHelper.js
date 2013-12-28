04e0692aa454bb8a1b8e0b4c816df01d68b1fa44
zhangchunsheng <peter@Wozllas-MacBook.local>
2013-12-28 14:22:11 +0800

    add weather report

--- titanium/shenglong-electricv/Resources/ui/helper/tabGroupHelper.js
+++ titanium/shenglong-electricv/Resources/ui/helper/tabGroupHelper.js
@@ -33,44 +33,99 @@ tabGroupHelper.createAppTabs = function(window) {
 
 tabGroupHelper.bindEvent = function(window) {
 	window.addEventListener("open", function(e) {
-		// window container
-		var welcomeWindow = Titanium.UI.createWindow({
-			height:80,
-			width:200,
-			touchEnabled:false
+		var date = new Date();
+		
+		// 天气预报、车辆先行、定位
+		var welcomeWindow = Ti.UI.createWindow({
+			title: '盛隆提醒',
+			backgroundImage: 'images/springFestival.jpg',
+			layout: 'vertical'
 		});
-
-		// black view
-		var indView = Titanium.UI.createView({
-			height:80,
-			width:200,
-			backgroundColor:'#000',
-			borderRadius:10,
-			opacity:0.8,
-			touchEnabled:false
+		//日期
+		var dateLabel = Ti.UI.createLabel({
+			color: '#fff',
+			font: {fontSize: 24},
+			showColor: '#aaa',
+			showOffset: {x:5, y:5},
+			shadowRadius: 3,
+			text: date.getFullYear() + "年" + date.getMonth() + "月" + date.getDate() + "日",
+			textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+			top: 10,
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
 		});
-		welcomeWindow.add(indView);
-
-		// message
-		var message = Titanium.UI.createLabel({
-			text:'盛隆电气欢迎您',
-			color:'#fff',
-			textAlign:'center',
-			font:{fontSize:18,fontWeight:'bold'},
-			height:'auto',
-			width:'auto'
+		welcomeWindow.add(dateLabel);
+		//天气情况
+		var situationImageLabel = Ti.UI.createLabel({
+			backgroundImage: 'images/weather/a0.jpg',
+			width: 48,
+			height: 48
 		});
-		welcomeWindow.add(message);
-		welcomeWindow.open();
-
-		var animationProperties = {delay: 1500, duration: 1000, opacity: 0.1};
-		if (Ti.Platform.osname == "iPhone OS") {
-			animationProperties.transform =
-				Ti.UI.create2DMatrix().translate(-200, 200).scale(0);
-		}
-		welcomeWindow.animate(animationProperties, function() {
+		welcomeWindow.add(situationImageLabel);
+		var situationTextLabel = Ti.UI.createLabel({
+			color: '#fff',
+			font: {fontSize: 24},
+			showColor: '#aaa',
+			showOffset: {x:5, y:5},
+			shadowRadius: 3,
+			text: '晴朗',
+			textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+			top: 30,
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		welcomeWindow.add(situationTextLabel);
+		
+		var temperatureLabel = Ti.UI.createLabel({
+			color: '#fff',
+			font: {fontSize: 60},
+			showColor: '#aaa',
+			showOffset: {x:5, y:5},
+			shadowRadius: 3,
+			text: '2度',
+			textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+			top: 10,
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		welcomeWindow.add(temperatureLabel);
+		
+		var todayLimitCarLabel = Ti.UI.createLabel({
+			color: '#fff',
+			font: {fontSize: 24},
+			showColor: '#aaa',
+			showOffset: {x:5, y:5},
+			shadowRadius: 3,
+			text: '星期六 限行尾号是：不限行',
+			textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+			top: 10,
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		welcomeWindow.add(todayLimitCarLabel);
+		
+		var tomorrowLimitCarLabel = Ti.UI.createLabel({
+			color: '#fff',
+			font: {fontSize: 24},
+			showColor: '#aaa',
+			showOffset: {x:5, y:5},
+			shadowRadius: 3,
+			text: '星期日 限行尾号是：不限行',
+			textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+			top: 10,
+			width: Ti.UI.SIZE,
+			height: Ti.UI.SIZE
+		});
+		welcomeWindow.add(tomorrowLimitCarLabel);
+		
+		enterBtn = Ti.UI.createButton({
+			title: '进入应用'
+		});
+		welcomeWindow.add(enterBtn);
+		enterBtn.addEventListener('click', function() {
 			welcomeWindow.close();
 		});
+		welcomeWindow.open({modal: true});
 	});
 };
 
