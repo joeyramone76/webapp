62e615e6bda13d4da6414072daaf87e546ac4953
zhangchunsheng <zhangchunsheng423@gmail.com>
2013-12-31 13:28:01 +0800

    add weather report

--- titanium/shenglong-electricv/Resources/utils/weatherUtil.js
+++ titanium/shenglong-electricv/Resources/utils/weatherUtil.js
@@ -4,5 +4,23 @@ weatherUtil.getCity = function() {
 	
 };
 
+weatherUtil.getLocation = function(callback) {
+	Titanium.Geolocation.getCurrentPosition(function(e) {
+		var longitude = 116.38926396691;
+		var latitude = 39.923697675933;
+		if(!e.success || e.error) {
+			longitude = 116.38926396691;
+			latitude = 39.923697675933;
+		}
+		longitude = e.coords.longitude;
+		latitude = e.coords.latitude;
+		callback({
+			longitude: longitude,
+			latitude: latitude
+		});
+	});
+};
+
 exports.weatherUtil = weatherUtil;
 exports.getCity = weatherUtil.getCity;
+exports.getLocation = weatherUtil.getLocation;
