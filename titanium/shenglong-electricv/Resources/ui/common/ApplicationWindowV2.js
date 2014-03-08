b72e74f18595ab086fa712e6d7dc5c1ac3014a50
zhangchunsheng <zhangchunsheng423@gmail.com>
2014-03-08 17:18:41 +0800

    4.重构：调整下拉刷新功能，更新背景

--- titanium/shenglong-electricv/Resources/ui/common/ApplicationWindowV2.js
+++ titanium/shenglong-electricv/Resources/ui/common/ApplicationWindowV2.js
@@ -132,122 +132,11 @@ function ApplicationWindow(opts) {
 	/**
 	 * webview pull to refresh
 	 */
-	var webviewSection = Ti.UI.createTableViewSection({
-		
-	});
-	var webviewRow = Ti.UI.createTableViewRow({
-		height: 'auto',
-		layout: 'vertical'
-	});
-	webviewRow.add(webview);
-	webviewSection.add(webviewRow);
-	var tableView = Ti.UI.createTableView({
+	var RefreshView = require("ui/common/view/RefreshView");
+	var refreshView = new RefreshView({
+		webview: webview,
 		top: config.top,
-		data: [webviewSection]
-	});
-	
-	var border = Ti.UI.createView({
-		backgroundColor: "#576c89",
-		height: 2,
-		bottom: 0
-	});
-	var tableHeader = Ti.UI.createView({
-		backgroundColor: "#e2e7ed",
-		width: 320,
-		height: 60
-	});
-	//bottom border 2 pixel
-	tableHeader.add(border);
-	
-	var arrow = Ti.UI.createView({
-		backgroundImage: "/images/whiteArrow.png",
-		width: 23,
-		height: 60,
-		bottom: 10,
-		left: 20
-	});
-	var statusLabel = Ti.UI.createLabel({
-		text: "下拉刷新页面",
-		left: 55,
-		width: 200,
-		bottom: 30,
-		height: "auto",
-		color: "#576c89",
-		textAlign: "center",
-		font: {fontSize:13,fontWeight:"bold"},
-		shadowColor: "#999",
-		shadowOffset: {x:0,y:1}
-	});
-	
-	var companyNameLabel = Ti.UI.createLabel({
-		text: "盛隆电气",
-		left: 55,
-		width: 200,
-		bottom: 15,
-		height: "auto",
-		color: "#576c89",
-		textAlign: "center",
-		font: {fontSize: 12},
-		shadowColor: "#999",
-		shadowOffset: {x:0,y:1}
-	});
-	
-	var actInd = Ti.UI.createActivityIndicator({
-		left: 20,
-		bottom: 13,
-		width: 30,
-		height: 30
-	});
-	tableHeader.add(arrow);
-	tableHeader.add(statusLabel);
-	tableHeader.add(companyNameLabel);
-	tableHeader.add(actInd);
-	
-	tableView.headerPullView = tableHeader;
-	var pulling = false;
-	var reloading = false;
-	
-	function beginReloading() {
-		// just mock out the reload
-		setTimeout(endReloading, 2000);
-	}
-	
-	function endReloading() {
-		// simulate loading
-		tableView.setContentInsets({top: 0}, {animated: true});
-		reloading = false;
-		statusLabel.text = "下拉刷新页面";
-		actInd.hide();
-		arrow.show();
-	}
-	
-	tableView.addEventListener('scroll', function(e) {
-		var offset = e.contentOffset.y;
-		if(offset <= -65.0 && !pulling && !reloading) {
-			var t = Ti.UI.create2DMatrix();
-			t = t.rotate(-180);
-			pulling = true;
-			arrow.animate({transform: t, duration: 180});
-			statusLabel.text = "释放即可刷新";
-		}
-	});
-	
-	var event_dragEnd = "dragEnd";
-	if(Ti.version >= '3.0.0') {
-		event_dragEnd = "dragend";
-	}
-	tableView.addEventListener(event_dragEnd, function(e) {
-		if(pulling && !reloading) {
-			reloading = true;
-			pulling = false;
-			arrow.hide();
-			actInd.show();
-			
-			statusLabel.text = "努力加载中...";
-			tableView.setContentInsets({top: 60}, {animated: true});
-			arrow.transform = Ti.UI.create2DMatrix();
-			beginReloading();
-		}
+		tabHeight: config.tabHeight
 	});
 	
 	/**
@@ -328,7 +217,7 @@ function ApplicationWindow(opts) {
 		//webview.setUrl(url);
 	});
 	
-	this.window.add(tableView);
+	this.window.add(refreshView.tableView);
 	this.window.add(loadView.loadView);
 	this.window.add(activityIndicator);
 	
